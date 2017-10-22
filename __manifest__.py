34ee85f5bc9e5e687feeddcfb7affb324e73484a
Toufiq <burningraven06@gmail.com>
2017-10-23 01:59:26 +0600

    pushing code

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "DietFacts",
+    'version': "11.0.1.0.0",
+    'author': "Haha",
+    'depends': ['sale'],
+    'installable': True,
+    'data': ['dietfacts_view.xml'],
+    'description': "Very Long Desription"
+}
---
+++ dietfacts.py
@@ -0,0 +1,34 @@
+from odoo import models, fields, api
+
+class Dietfacts_product_template(models.Model):
+	_name='product.template'
+	_inherit='product.template'
+	calories = fields.Integer("Calories")
+	serving_size = fields.Float("Serving Size")
+	last_updated = fields.Date("Last Updated")
+	# diet_item = fields.Boolean("Diet Item")
+
+class Dietfacts_res_users_meal(models.Model):
+	_name='res.users.meal'
+	name=fields.Char('Meal Name')
+	meal_date = fields.Datetime("Meal Date")
+	item_ids = fields.One2many('res.users.mealitem', 'meal_id')
+	total_calories = fields.Integer(string='Meal Total-Calories', store=True, compute='_sumcalories')
+	user_id = fields.Many2one('res.users', 'Meal Eater')
+	notes = fields.Char("Meal Notes")
+
+	@api.one
+	@api.depends('item_ids', 'item_ids.servings')
+	def _sumcalories(self):
+		sum_calories = 0
+		for meal_item in self.item_ids:
+			sum_calories += (meal_item.item_calories*meal_item.servings)
+		self.total_calories = sum_calories
+
+class Dietfacts_res_users_mealitem(models.Model):
+	_name='res.users.mealitem'
+	meal_id = fields.Many2one('res.users.meal', 'Meal')
+	item_id = fields.Many2one('product.template', 'DietProduct')
+	item_calories = fields.Integer(related='item_id.calories', string='MealItem Calorie', store='True', readonly='True')
+	servings = fields.Float('Servings')
+	notes=  fields.Text("Meal Item Notes")
