1c378fb98823ff12cd0cb85e8746422c3e3fac7a
Toufiq <burningraven06@gmail.com>
2018-01-07 00:04:16 +0600

    xmlprc request works

--- importproducts.py
+++ importproducts.py
@@ -1,1 +1,7 @@
 import xmlrpclib, csv
+server = 'http://localhost:8069'
+database = 'odoo10db'
+user = 'user@test.com'
+password = 'test1234'
+common = xmlrpclib.ServerProxy('%s/xmlrpc/2/common' %server)
+print common.version()
