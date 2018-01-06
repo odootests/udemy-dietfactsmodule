56de0072864b7d08a2cd5c5fa6adc4ae95572c25
Toufiq <burningraven06@gmail.com>
2018-01-07 00:59:18 +0600

    odoo api, database connection made, user authentication done, search_count from models done

--- importproducts.py
+++ importproducts.py
@@ -1,7 +1,19 @@
 import xmlrpclib, csv
-server = 'http://localhost:8069'
+
+host = 'http://localhost:8069'
 database = 'odoo10db'
-user = 'user@test.com'
+username = 'test@user.com'
 password = 'test1234'
-common = xmlrpclib.ServerProxy('%s/xmlrpc/2/common' %server)
+
+common = xmlrpclib.ServerProxy('%s/xmlrpc/2/common' %host)
 print common.version()
+
+user_id = common.authenticate(database, username, password, {})
+print("User ID: %s" %user_id)
+
+odoo_api = xmlrpclib.ServerProxy('%s/xmlrpc/2/object' %host)
+product_count = odoo_api.execute_kw(database, user_id, password, 'product.template', 'search_count', [[]])
+user_count = odoo_api.execute_kw(database, user_id, password, 'res.users', 'search_count', [[]])
+
+print('Products: %s' %product_count)
+print('Users: %s' %user_count)
