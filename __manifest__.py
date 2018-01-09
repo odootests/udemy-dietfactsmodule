37f1a42732fb6676be6730878cfbb8d0c1945e0e
Toufiq <burningraven06@gmail.com>
2018-01-09 18:09:10 +0600

    new report added, report on separte file/modular method

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,6 @@
     'author': "Haha2",
     'depends': ['sale'],
     'installable': True,
-    'data': ['dietfacts_product_view.xml', 'dietfacts_meal_view.xml', 'dietfacts_nutrient_view.xml', 'dietfacts_drink_view.xml', 'dietfacts_nutrient_report_view.xml'],
+    'data': ['dietfacts_product_view.xml', 'dietfacts_meal_view.xml', 'dietfacts_nutrient_view.xml', 'dietfacts_drink_view.xml', 'dietfacts_nutrient_report_view.xml', 'dietfacts_meal_report_view.xml'],
     'description': "Very Long Desription"
 }
