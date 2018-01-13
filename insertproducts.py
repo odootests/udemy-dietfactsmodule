dcbe5b931c3ddb3dd026431fb4df09619721a2fb
Toufiq <burningraven06@gmail.com>
2018-01-13 15:48:52 +0600

    retrieving field value with api and adding them to record

--- insertproducts.py
+++ insertproducts.py
@@ -14,16 +14,19 @@ filename = "import_data.csv"
 
 reader = csv.reader(open(filename, 'rb'))
 
+filter_by_categname = [[('name', '=', "Diet Items")]]
+product_categ_id = odoo_api.execute_kw(database, user_id, password, 'product.category', 'search', filter_by_categname)
+
 for row in reader:
 	product_name = row[0]
 	product_calorie = row[1]
 
-	filter_byname = [[('name', '=', product_name)]]
-	existing_product = odoo_api.execute_kw(database, user_id, password, 'product.template', 'search', filter_byname)
+	filter_by_name = [[('name', '=', product_name)]]
+	existing_product = odoo_api.execute_kw(database, user_id, password, 'product.template', 'search', filter_by_name)
 	if existing_product:
-		print("Product Exists - ID: %s" %str(existing_product))
+		print("Product Exists! ID: %s" %str(existing_product))
 	else:
-		record = [{'name': product_name, 'calories':product_calorie}]
+		record = [{'name': product_name, 'calories':product_calorie, 'categ_id':product_categ_id[0]}]
 		odoo_api.execute_kw(database, user_id, password, 'product.template', 'create', record)
-		print("Added Product Name: %s and Calories: %s" %(product_name, product_calorie))
+		print("Added Product Name: %s, Calories: %s, Prod-Category" %(product_name, product_calorie, product_categ_id[0]))
 
